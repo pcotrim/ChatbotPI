b6cc3cf4b042f580bfdd7f35c6f2e9c68ff440cd
pcotrim <pcotrimdeabrantes@gmail.com>
2024-12-04 09:21:26 -0300

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://cientistaspatentes.com.br/imagens/paula.png" >
+        <img src="https://cientistaspatentes.com.br/imagens/avatar2.png" >
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
