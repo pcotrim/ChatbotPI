2ed9f8a9f13e79675d3ddd1c341619ca26631a48
pcotrim <97361761+pcotrim@users.noreply.github.com>
2025-10-20 10:19:02 -0300

    Update chatbotpi.py

--- chatbotpi.py
+++ chatbotpi.py
@@ -4,7 +4,7 @@ from htmlTemplates import css, bot_template, user_template
 #from langchain.chains import ConversationChain
 #from langchain.memory import ConversationBufferMemory
 #from langchain.chat_models import ChatOpenAI
-from langchain_community.chains import ConversationChain
+#from langchain_community.chains import ConversationChain
 #from langchain_openai import ChatOpenAI
 #from langchain_core.prompts import PromptTemplate
 #from langchain.memory import ConversationBufferMemory
